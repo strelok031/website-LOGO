e886138161914b472794e2518f9f67c6157d24a1
Vova <he.beite.pliz@gmail.com>
2022-10-30 13:16:55 +0200

    web-site LOGO
    web-site LOGO

---
+++ js/burger.js
@@ -0,0 +1,13 @@
+﻿const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+   hamburger.classList.toggle("active");
+   navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+   addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+   }))
